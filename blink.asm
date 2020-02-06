126b2c40466842fbc14bdc7016fb96ced5beeafd
mati1000500100900 <mati1000500100900@gmail.com>
2020-02-06 22:47:50 +0100

    Blin fix

--- blink.asm
+++ blink.asm
@@ -4,30 +4,27 @@
 
 setup:	
 	ldi r16, 0b00100000		
-	out DDRB, r16				; set PB5 (aduino 13) as output
+	out DDRB, r16					; set PB5 (aduino 13) as output
 loop:
-	ldi r16, 0b00100000
-	out PORTB, r16				; set PB5 HIGH
+	sbi PORTB, PB5					; set PB5 HIGH
+	rcall delay					; wait 500ms
 	
-	rcall delay				; wait 500ms
-	
-	clr r16
-	out PORTB, r16				; set PB5 LOW
-	
-	rcall delay				; wait 500ms
+	cbi PORTB, PB5					; set PB5 LOW
+	rcall delay					; wait 500ms
+
 	rjmp loop
 
 delay:
 	ser r17
 	ser r18
-	ldi r19, 30				; set delay duration
+	ldi r19, 30					; set delay duration
 waste:
 	nop
 	dec r17
 	brne waste
 	dec r18
 	brne waste
-	dec r19					; loop set amount of times
-	brne waste				; in this case 256*256*30 for roughly 500ms delay
-ret
+	dec r19						; loop set amount of times
+	brne waste					; in this case 256*256*30 for roughly 500ms delay
+	ret
 	
